bad4c4fcfb7ddd2d78306bc89ea83377e1fb0039
Daniel Paul <333233+danielpaul@users.noreply.github.com>
2025-08-08 18:27:43 +0100

    migrate

--- config/initializers/clerk.rb
+++ config/initializers/clerk.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Clerk.configure do |c|
-  c.secret_key = Rails.application.credentials.dig(:clerk_secret_key) || ENV["CLERK_SECRET_KEY"] || "sk_test_1234567890"
-  c.publishable_key = Rails.application.credentials.dig(:clerk_publishable_key) || ENV["CLERK_PUBLISHABLE_KEY"] || "pk_test_1234567890"
+  c.secret_key = Rails.application.credentials.dig(:clerk, :secret_key) || ENV["CLERK_SECRET_KEY"] || "sk_test_1234567890"
+  c.publishable_key = Rails.application.credentials.dig(:clerk, :publishable_key) || ENV["CLERK_PUBLISHABLE_KEY"] || "pk_test_1234567890"
   c.logger = Rails.logger if Rails.env.development?
 end
