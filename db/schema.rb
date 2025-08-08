c0281a07fa118287629902a04238aa8562c73db5
Daniel Paul <333233+danielpaul@users.noreply.github.com>
2025-08-08 14:00:54 +0100

    tools versions & use PG

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_11_160811) do
     t.string "ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,6 +28,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_11_160811) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  add_foreign_key "sessions", "users"
 end
